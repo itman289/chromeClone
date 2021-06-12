a84a47475cb14bdd90f29e3dbe63665e492e2714
Your Name <you@example.com>
2021-06-13 02:39:25 +0900

    imgUpload

--- bg.js
+++ bg.js
@@ -2,13 +2,19 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 3;
 
+function handleImgLoad() {
+  console.log("finished loading");
+}
+
 function paintImage(imgNumber) {
   const image = new Image();
-  img.src = `/images/${imgNumber + 1}.jpg`;
+  image.src = `images/${imgNumber + 1}.jpg`;
+  image.classList.add("bgImage");
+  body.prepend(image);
 }
 
 function genRandom() {
-  const number = Math.floor(Math.random * IMG_NUMBER);
+  const number = Math.floor(Math.random() * IMG_NUMBER);
   return number;
 }
 
