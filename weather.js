a9dd6c45ee6948dbb8bbeced0fdd378f865a349b
Your Name <you@example.com>
2021-06-14 01:11:31 +0900

    wh

---
+++ weather.js
@@ -0,0 +1,33 @@
+const COORDS = "coords";
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+}
+
+function handleGeoError() {
+  console.log("location can't find");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCords = localStorage.getItem(COORDS);
+  if (loadedCords === null) {
+    askForCoords();
+  } else {
+    // getweather
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
